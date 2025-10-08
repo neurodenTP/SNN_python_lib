f0053472a1fbfaaa0fd18e15739d30426db385b1
Nikita <67433712+NikomSol@users.noreply.github.com>
2025-10-08 15:44:05 +0300

    New structure
    Neuron and Synapse - rule
    Layer and Connection - variables have

--- src/optimization.py
+++ src/optimization.py
@@ -8,11 +8,17 @@ class STDP:
         self.A_minus = params.get('A_minus', 0.012)
         self.tau_plus = params.get('tau_plus', 20.0)
         self.tau_minus = params.get('tau_minus', 20.0)
+        self.y_plus = None
+        self.y_minus = None
+        
 
-    def rule(self, weights, pre_outputs_func, post_outputs_func):
+    def rule(self, weights, pre_spikes, post_spikes, dt):
         weights_delta = np.zeros_like(weights)
         pre_spikes = pre_outputs_func()
         post_spikes = post_outputs_func()
+
+        y_plus *= (1 - dt/self.tau_plus)
+        y_minus *= (1 - dt/self.tau_minus)
 
         for i_post, post_val in enumerate(post_spikes):
             for j_pre, pre_val in enumerate(pre_spikes):
@@ -25,25 +31,4 @@ class STDP:
         np.clip(weights, 0, 1, out=weights)
         return weights
 
-class STDPWithDecay:
-    def __init__(self, params):
-        self.A_plus = params.get('A_plus', 0.01)
-        self.A_minus = params.get('A_minus', 0.012)
-        self.tau_plus = params.get('tau_plus', 20.0)
-        self.tau_minus = params.get('tau_minus', 20.0)
-        self.decay_rate = params.get('decay_rate', 0.001)
-
-    def rule(self, weights, pre_outputs_func, post_outputs_func):
-        weights_delta = np.zeros_like(weights)
-        pre_spikes = pre_outputs_func()
-        post_spikes = post_outputs_func()
-
-        for i_post, post_val in enumerate(post_spikes):
-            for j_pre, pre_val in enumerate(pre_spikes):
-                if pre_val and post_val:
-                    weights_delta[i_post, j_pre] += self.A_plus - self.A_minus
-
-        weights += weights_delta
-        weights *= (1 - self.decay_rate)
-        np.clip(weights, 0, 1, out=weights)
-        return weights
+class LTPf
