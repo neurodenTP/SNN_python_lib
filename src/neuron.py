95151a01e9e4274c66823cdb39ad6c78061a257a
Nikita <67433712+NikomSol@users.noreply.github.com>
2025-10-09 18:34:27 +0300

    neuron upd

--- src/neuron.py
+++ src/neuron.py
@@ -2,28 +2,46 @@ import numpy as np
 
 class Neuron:
     def __init__(self, name, N, params):
+        """
+        Инициализация базового нейрона.
+
+        Аргументы:
+        params (dict): словарь параметров нейрона, включает:
+            'Ustart' (float): начальное значение мембранного потенциала
+            'Istart' (float): начальный выходной ток
+            'Sstart' (bool): начальное состояние
+            
+        """
         self.N = N
-        self.params = params
+        self.params = params.copy()
         
-        for key in ['Ustart', 'Istart']:
+        self.check_params(['Ustart', 'Istart', 'Sstart'])
+        
+        self.ustart = self.params['Ustart']
+        self.istart = self.params['Istart']
+        self.sstart = self.params['Sstart']
+        
+        self.U = np.empty(N)
+        self.I = np.empty(N)
+        
+        self.U = self.ustart.copy()
+        self.I = self.istart.copy()
+        self.S = self.sstart.copy()
+
+    def check_params(self, keys):
+        N = self.N
+        for key in keys:
             if key not in self.params:
                 raise ValueError(f"В словаре params нет {key}")
             if not hasattr(self.params[key], '__len__'):
                 self.params[key] = np.full(N, self.params[key])
             if len(self.params[key]) != N:
                 raise ValueError(f"Длина параметра {key} = {len(self.params[key])} не соответствует количеству нейронов ({N})")    
-        
-        self.ustart = params['Ustart']
-        self.istart = params['Istart']
-        
-        self.U = np.empty(N)
-        self.I = np.empty(N)
-        
-        self.reset()
 
     def reset(self):
-        self.U = self.ustart
-        self.I = self.istart
+        self.U = self.ustart.copy()
+        self.I = self.istart.copy()
+        self.S = self.sstart.copy()
 
     def step(self, dt, Iin):
         pass
@@ -33,131 +51,174 @@ class Neuron:
     
     def get_current(self):
         return self.I
+    
+    def get_spike(self):
+        return self.S
 
 
 class LIFNeuron(Neuron):
-    def __init__(self, params):
+    def __init__(self, name, N, params):
         """
         Инициализация LIF нейрона.
 
         Аргументы:
         params (dict): словарь параметров нейрона, включает:
-            'Ustart' (float): начальное значение мембранного потенциала (по умолчанию 0.0)
-            'Utay' (float): постоянная времени мембранного потенциала (декадация), в тех же единицах времени (по умолчанию 10.0)
-            'Uth' (float): порог спайка мембранного потенциала (по умолчанию 1.0)
-            'Urest' (float): потенциал покоя мембраны после спайка (по умолчанию 0.0)
+            'Ustart' (float): начальное значение мембранного потенциала
+            'Utay' (float): постоянная времени мембранного потенциала (декадация), в тех же единицах времени
+            'Uth' (float): порог спайка мембранного потенциала
+            'Urest' (float): потенциал покоя мембраны после спайка
             
-            'Itay' (float): постоянная времени выхода тока (декадация), в тех же единицах времени (по умолчанию 10.0)
-            'Imax' (float): максимальный выходной ток при спайке (по умолчанию 1.0)
-            'Istart' (float): начальный выходной ток (по умолчанию 0.0)
+            'Itay' (float): постоянная времени выхода тока (декадация), в тех же единицах времени
+            'Imax' (float): максимальный выходной ток при спайке
+            'Istart' (float): начальный выходной ток
+            
+            'Sstart' (bool): начальное состояние
+            
         """
-        super().__init__(params)
+        super().__init__(name, N, params)
         
-        self.utay = params.get('Utay', 10.0)
-        self.uth = params.get('Uth', 1.0)
-        self.urest = params.get('Urest', 0.0)
-        self.ustart = params.get('Ustart', self.urest)
+        self.check_params(['Utay', 'Uth', 'Urest',
+                           'Itay', 'Imax'])
         
-        self.itay = params.get('Itay', 10.0)
-        self.imax = params.get('Iout_max', 1.0)
-        self.istart = params.get('Istart', 0.0)
-
-
-    def generate(self, N):
-        var = {'U': np.full(N, self.ustart),
-               'I': np.full(N, self.istart),
-               'S': np.full(N, 0)}
-        return var
-
-    def reset(self, var):
-        var['U'].fill(self.ustart)
-        var['I'].fill(self.istart)
-        var['S'].fill(0.0)
-
-    def step(self, var, dt, Iin):
-        var['U'] *= (1 - dt / self.utay)
-        var['U'] += Iin
+        self.utay = self.params['Utay']
+        self.uth = self.params['Uth']
+        self.urest = self.params['Urest']
         
-        ind_spike = np.where(var['U'] >= self.uth)
-        ind_no_spike = np.where(var['U'] < self.uth)
+        self.itay = self.params['Itay']
+        self.imax = self.params['Imax']
 
-        var['I'][ind_spike] = self.imax
-        var['U'][ind_spike] = self.urest
-        var['S'][ind_spike] = 1.
+    def step(self, dt, Iin):
+        self.U *= (1 - dt / self.utay)
+        self.U += Iin
         
-        var['I'][ind_no_spike] *= (1 - dt / self.itay)
-        var['S'][ind_no_spike] = 0.
+        self.S = self.U >= self.uth
+        ind_spike = np.where(self.S)
+        ind_no_spike = np.where(np.invert(self.S))
+
+        self.I[ind_spike] = self.imax[ind_spike]
+        self.U[ind_spike] = self.urest[ind_spike]
+        
+        self.I[ind_no_spike] *= (1 - dt / self.itay[ind_no_spike])
 
 
             
 class AdaptiveLIFNeuron(Neuron):
-    def __init__(self, params):
+    def __init__(self, name, N, params):
         """
         Инициализация LIF Adaptive нейрона.
 
         Аргументы:
         params (dict): словарь параметров нейрона, включает:
-            'Utay' (float): постоянная времени мембранного потенциала (декадация), в тех же единицах времени (по умолчанию 10.0)
-            'Uth' (float): порог спайка мембранного потенциала (по умолчанию 1.0)
-            'Ustart' (float): начальное значение мембранного потенциала (по умолчанию 0.0)
+            'Utay' (float): постоянная времени мембранного потенциала (декадация), в тех же единицах времени
+            'Uth' (float): порог спайка мембранного потенциала
+            'Ustart' (float): начальное значение мембранного потенциала
             
-            'Vtay' (float): постоянная времени потенциала восстановления, в тех же единицах времени (по умолчанию 10.0)
+            'Vtay' (float): постоянная времени потенциала восстановления, в тех же единицах времени
             'Vstep' (float): шаг изменения потенциала восстановления при активации
-            'Vstart' (float): потенциала восстановления (по умолчанию 0.0)
+            'Vstart' (float): потенциала восстановления
             
-            'Itay' (float): постоянная времени выхода тока (декадация), в тех же единицах времени (по умолчанию 10.0)
-            'Imax' (float): максимальный выходной ток при спайке (по умолчанию 1.0)
-            'Istart' (float): начальный выходной ток (по умолчанию 0.0)
+            'Itay' (float): постоянная времени выхода тока (декадация), в тех же единицах времени
+            'Imax' (float): максимальный выходной ток при спайке
+            'Istart' (float): начальный выходной ток
         """
-        super().__init__(params)
+        super().__init__(name, N, params)
+
+        self.check_params(['Utay', 'Uth',
+                           'Vtay', 'Vstep','Vstart',
+                           'Itay', 'Imax'])
         
-        self.utay = params.get('Utay', 10.0)
-        self.uth = params.get('Uth', 1.0)
-        self.ustart = params.get('Ustart', self.urest)
         
-        self.vtay = params.get('Vtay', 1000.0)
-        self.vstep = params.get('Vstep', 0.1)
-        self.vstart = params.get('Vstart', 0.0)
+        self.utay = self.params['Utay']
+        self.uth = self.params['Uth']
         
-        self.itay = params.get('Itay', 10.0)
-        self.imax = params.get('Imax', 1.0)
-        self.istart = params.get('Istart', 0.0)
-
-    def generate(self, N):
-        var = {'U': np.full(N, self.ustart),
-               'V': np.full(N, self.vstart),
-               'I': np.full(N, self.istart),
-               'S': np.full(N, 0)}
-        return var
-
-    def reset(self, var):
-        var['U'].fill(self.vstart)
-        var['V'].fill(self.ustart)
-        var['I'].fill(self.istart)
-        var['S'].fill(0)
-
-    def step(self, var, dt, Iin):
-        var['U'] *= (1 - dt / self.utay)
-        var['U'] += Iin
+        self.vtay = self.params['Vtay']
+        self.vstep = self.params['Vstep']
+        self.vstart = self.params['Vstart'].copy()
         
-        ind_spike = np.where(var['U'] >= self.uth)
-        ind_no_spike = np.where(var['U'] < self.uth)
-
-        var['I'][ind_spike] = self.imax
-        var['V'][ind_spike] -= self.vstep
-        var['U'][ind_spike] = 1.0 * var['V'][ind_spike]
-        var['S'][ind_spike] = 1.
+        self.itay = self.params['Itay']
+        self.imax = self.params['Imax']
         
-        var['I'][ind_no_spike] *= (1 - dt / self.itay)
-        var['V'][ind_no_spike] *= (1 - dt / self.vtay)
-        var['S'][ind_no_spike] = 0.
+        self.reset()
+
+    def reset(self):
+        self.U = self.ustart.copy()
+        self.V = self.vstart.copy()
+        self.I = self.istart.copy()
+        self.S = self.sstart.copy()
+
+    def step(self, dt, Iin):
+        self.U *= (1 - dt / self.utay)
+        self.U += Iin
+        
+        self.S = self.U >= self.uth
+        ind_spike = np.where(self.S)
+        ind_no_spike = np.where(np.invert(self.S))
+
+        self.I[ind_spike] = self.imax[ind_spike]
+        self.V[ind_spike] -= self.vstep[ind_spike]
+        self.U[ind_spike] = self.V.copy()[ind_spike]
+        
+        self.I[ind_no_spike] *= (1 - dt / self.itay[ind_spike])
+        self.U[ind_no_spike] *= (1 - dt / self.vtay[ind_spike])
 
 
-if __name__ == '__main__':
-    N = 4
-    params = {'Ustart': np.zeros(N),
-              'Istart': 0.}
-    neuron = Neuron('kwa', N, params)
-    print(neuron.get_current())
-    print(neuron.get_potential())
+# if __name__ == '__main__':
+    # N = 4
+    # params = {'Ustart': np.zeros(N),
+    #           'Istart': 0.,
+    #           'Sstart': False}
+    # neuron = Neuron('kwa', N, params)
+    # print(neuron.get_current())
+    # print(neuron.get_potential())
+    # print(neuron.get_spike())
     
+    # N = 4
+    # params = {'Ustart': np.zeros(N),
+    #           'Istart': 0.0,
+    #           'Sstart': False,
+    #           'Utay': 2., 'Uth': 1.0, 'Urest': 0.,
+    #           'Itay': 100., 'Imax': 1.0}
+    # neuron = LIFNeuron('kwa', N, params)
+    
+    # neuron.step(1, 0.4 * np.arange(0, N))
+    # print(neuron.get_potential())
+    # print(neuron.get_current())
+    # print(neuron.get_spike())
+    
+    # neuron.step(1, 0.4 * np.arange(0, N))
+    # print(neuron.get_potential())
+    # print(neuron.get_current())
+    # print(neuron.get_spike())
+    
+    # neuron.step(1, 0.4 * np.arange(0, N))
+    # print(neuron.get_potential())
+    # print(neuron.get_current())
+    # print(neuron.get_spike())
+    
+    # N = 4
+    # params = {'Ustart': 0.0,
+    #           'Vstart': 0.0,
+    #           'Istart': 0.0,
+    #           'Sstart': 0.0,
+    #           'Utay': 2., 'Uth': 1.0,
+    #           'Vtay': 10., 'Vstep': 1.0,
+    #           'Itay': 100., 'Imax': 1.0}
+    # neuron = AdaptiveLIFNeuron('kwa', N, params)
+    
+    # neuron.step(1, 0.4 * np.arange(0, N))
+    # print(neuron.get_potential())
+    # print(neuron.V)
+    # print(neuron.get_current())
+    # print(neuron.get_spike())
+    
+    # neuron.step(1, 0.4 * np.arange(0, N))
+    # print(neuron.get_potential())
+    # print(neuron.V)
+    # print(neuron.get_current())
+    # print(neuron.get_spike())
+    
+    # neuron.step(1, 0.4 * np.arange(0, N))
+    # print(neuron.get_potential())
+    # print(neuron.V)
+    # print(neuron.get_current())
+    # print(neuron.get_spike())
