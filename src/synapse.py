8c7935770f8afe44eff39c531f3859c65b500e2e
Nikita <67433712+NikomSol@users.noreply.github.com>
2025-10-10 17:00:33 +0300

    learning concept

--- src/synapse.py
+++ src/synapse.py
@@ -6,11 +6,13 @@ class Synapse:
     Хранит веса связей и реализует передачу сигнала.
     """
 
-    def __init__(self, name: str, preNeuron, postNeuron, weight: np.ndarray = None):
+    def __init__(self, name: str, preNeuron, postNeuron,
+                 weight: np.ndarray = None, params = None):
         self.name = name
         self.pre = preNeuron
         self.post = postNeuron
         self.weight_shape = (self.post.N, self.pre.N)
+        self.params = params
 
         if weight is None:
             self.weight = self.generate_random_weight()
@@ -48,3 +50,51 @@ class Synapse:
             if new_weight.shape != self.weight_shape:
                 raise ValueError(f"Форма new_weight ({new_weight.shape}) должна совпадать с формой weight ({self.weight_shape})")
             self.weight = new_weight
+    
+    def check_params(self, keys: list[str]):
+        for key in keys:
+            if key not in self.params:
+                raise ValueError(f"В словаре params нет {key}")
+            
+
+class SynapseSTDP(Synapse):
+    def __init__(self, name: str, preNeuron, postNeuron,
+                 weight: np.ndarray = None, params = None):
+        super().__init__(name, preNeuron, postNeuron, weight, params)
+        
+        self.check_params(['Aplus', 'Aminus', 'Tplus', 'Tminus'])
+        self.a_plus = self.params['Aplus']
+        self.a_minus = self.params['Aminus']
+        self.tay_plus = self.params['Tplus']
+        self.tay_minus = self.params['Tminus']
+        
+        self.pre_trace = np.zeros(self.weight_shape[1])
+        self.post_trace = np.zeros(self.weight_shape[0])
+    
+    def update_weight(self, dt: float):
+        """Обновление весов синапса"""
+        
+        """
+        Матрично
+        идем по пре, обновляем все следы * и прибавить если спайк
+        идет по пост, обновляем все следы * и прибавить если спайк
+        
+        Итак имеем следы.
+        
+        умножаем спайки пост на след пре на А - прибавляем к весу
+        умножааем спайки пре на след пост на А - вычитаем 
+        """
+        
+class SynapseLTPf(Synapse):
+    def update_weight(self, dt: float):
+        """Обновление весов синапса"""
+        
+        """
+        Матрично
+        идем по пре, обновляем все следы * и прибавить если спайк
+        
+        Итак имеем следы.
+        
+        умножаем спайки пост на след пре на А - прибавляем к весу
+        вычитаем на фиксированное число
+        """
